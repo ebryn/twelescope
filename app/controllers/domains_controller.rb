4a5b83b572af580c0967cbd8dc6bcd00b35c5346
Erik Bryn <ebryn@marketmonitors.com>
2009-08-29 18:58:27 -0700

    more domains stuff

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -7,4 +7,14 @@ class DomainsController < ApplicationController
       :group => "domain", :order => "COUNT(*) DESC"
     )
   end
+  
+  def show
+    @domain = params[:id].gsub('_', '.')
+    @links = Link.paginate(:page => params[:page], 
+      :select => "links.id, domain, page_title, expanded_url, url, COUNT(*) AS linkages_count",
+      :order => "COUNT(*) DESC, links.id",
+      :group => "links.id, domain, page_title, expanded_url, url",
+      :joins => "JOIN linkages ON links.id = linkages.link_id",
+      :conditions => {:domain => @domain})
+  end
 end
