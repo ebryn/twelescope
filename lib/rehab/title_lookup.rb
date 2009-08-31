1db5548d2f15d3593296d087d19190bc7da95441
Austin Putman <austin@radicaldesigns.org>
2009-08-30 19:09:12 -0700

    dont provide titles for non-web-pages

--- lib/rehab/title_lookup.rb
+++ lib/rehab/title_lookup.rb
@@ -2,6 +2,8 @@ module Rehab
   class TitleLookup
     include Typhoeus
     remote_defaults :on_success => lambda {|response| 
+       headers = HashWithIndifferentAccess[ *response.headers.split( "\r\n" ).delete_if { |h| h =~ /^HTTP/ }.map { |x| x.split(": ",2) }.flatten ]
+       return nil unless headers['Content-Type'] =~ /(xml|html)/
       Nokogiri.HTML( response.body ).at("//title").text.strip rescue nil
     },
      :on_failure => lambda {|response| 
